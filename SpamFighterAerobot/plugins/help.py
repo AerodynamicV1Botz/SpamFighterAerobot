1313ab9a04e0065bed504c33dc0aaf5fca7fe383
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2022-10-07 12:24:21 +0530

    Update help.py

--- SpamFighterAerobot/plugins/help.py
+++ SpamFighterAerobot/plugins/help.py
@@ -7,25 +7,25 @@ from SpamFighterAerobot import CMD_HNDLR as hl
     
 HELP_PIC = "https://te.legra.ph/file/8fa034b5a6faae2c9da16.jpg"
 
-DEAD_Help = "🔥 **SpamFighterAerobot** 🔥\n\n"
+AERO_Help = "🔥 **SpamFighterAerobot** 🔥\n\n"
  
-DEAD_Help += f"__ᴄᴍɴᴅs ᴀᴠᴀɪʟᴀʙʟᴇ ɪɴ SpamFighterAerobot__\n\n"
+AERO_Help += f"__ᴄᴍɴᴅs ᴀᴠᴀɪʟᴀʙʟᴇ ɪɴ SpamFighterAerobot__\n\n"
 
-DEAD_Help += f" ↧ 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 𝙲𝙼𝙳𝚂 ↧\n\n"
+AERO_Help += f" ↧ 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 𝙲𝙼𝙳𝚂 ↧\n\n"
 
-DEAD_Help += f" /ping - to check ping\n /alive - to check bot alive/version (only main userbot will reply)\n /restart - to restart all spam bots \n /addecho - to addecho \n /rmecho - To remove Echo \n /addsudo - To add sudo user using spam bot \n\n"
+AERO_Help += f" /ping - to check ping\n /alive - to check bot alive/version (only main userbot will reply)\n /restart - to restart all spam bots \n /addecho - to addecho \n /rmecho - To remove Echo \n /addsudo - To add sudo user using spam bot \n\n"
  
-DEAD_Help += f" ↧ 𝙻𝙴𝙰𝚅𝙴 𝙲𝙼𝙳 ↧\n\n"
+AERO_Help += f" ↧ 𝙻𝙴𝙰𝚅𝙴 𝙲𝙼𝙳 ↧\n\n"
 
-DEAD_Help += f" /leave - to leave public/private channel/groups\n\n"
+AERO_Help += f" /leave - to leave public/private channel/groups\n\n"
  
-DEAD_Help += f" ↧ 𝚂𝙿𝙰𝙼 𝙲𝙼𝙳𝚂 ↧\n\n"
+AERO_Help += f" ↧ 𝚂𝙿𝙰𝙼 𝙲𝙼𝙳𝚂 ↧\n\n"
 
-DEAD_Help += f" /raid - to raid\n replyraid - to active reply raid\n /dreplyraid - to de-active reply raid\n /spam - this cmd use for Normal spam\n /bigspam - this cmd use for big spam\n /uspam - this cmd use for unlimited Spam until You restart the bots!!\n /delayspam - this cmd use for delay spam\n\n"
+AERO_Help += f" /raid - to raid\n replyraid - to active reply raid\n /dreplyraid - to de-active reply raid\n /spam - this cmd use for Normal spam\n /bigspam - this cmd use for big spam\n /uspam - this cmd use for unlimited Spam until You restart the bots!!\n /delayspam - this cmd use for delay spam\n\n"
 
-DEAD_Help += f" /aerospam - ɪ ᴡɪʟʟ ꜱᴜɢɢᴇꜱᴛ ᴅᴏɴ'ᴛ ᴜꜱᴇ ᴛʜɪꜱ ᴄᴏᴍᴍᴀɴᴅ😂 ↧\n\n"
+AERO_Help += f" /aerospam - ɪ ᴡɪʟʟ ꜱᴜɢɢᴇꜱᴛ ᴅᴏɴ'ᴛ ᴜꜱᴇ ᴛʜɪꜱ ᴄᴏᴍᴍᴀɴᴅ😂 ↧\n\n"
 
-DEAD_Help += f"© @AerodynamicV1_Update\n"
+AERO_Help += f"© @AerodynamicV1_Promotion\n"
 
 
 @BOT0.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
@@ -33,7 +33,7 @@ async def help(event):
     if event.sender_id in SUDO_USERS:
       await BOT0.send_file(event.chat_id,
                                   HELP_PIC,
-                                  caption=DEAD_Help,
+                                  caption=AERO_Help,
                                   buttons=[
         [
         Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/AerodynamicV1_Update")
